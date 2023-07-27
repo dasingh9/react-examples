043174b10b2207202910bb41a1fe665faab23180
Davinder Singh <davinder.singh@auckland.ac.nz>
2023-07-27 21:07:48 +1200

    Add GamesFilter component

--- react-vite-example-basics/src/games-list/GamesList.jsx
+++ react-vite-example-basics/src/games-list/GamesList.jsx
@@ -1,22 +1,63 @@
+import { useState } from "react";
 import GameCard from "../game-card/GameCard";
 
 function GamesList (props) {
-    const games = props.games;
-
+    const originalGamesArray = props.games;
+    const [games, setGames] = useState([...originalGamesArray]);
 
     let gamesJsx = games.map(game => (
-        <div className="col-lg-3">
+        <div className="col-lg-3" key={game.id}>
             <GameCard game = {game}></GameCard>
         </div>
         )
     );
 
+    const filterGameByTitle = function (title) {
+        if(title.length >=3) {
+            let filteredGames = originalGamesArray.filter(game => game.title.toLowerCase().includes(title.toLowerCase()));
+            setGames(filteredGames);
+        }
+        else
+            setGames(originalGamesArray);
+    }
+
+
+    const filterGameByGenre = function (genre) {
+        let filteredGames = originalGamesArray.filter(game => game.title.toLowerCase().includes(title.toLowerCase()));
+        setGames(filteredGames);
+    }
 
     return (
         <>
+        <GamesFilter onTitleChange={filterGameByTitle}></GamesFilter>
+
         <div className="row">
             {gamesJsx}
          </div>  
+        </>
+    );
+}
+
+function GamesFilter({onTitleChange, onGenreChange}) {
+    const [title, setTitle] = useState("");
+
+    function handleTitleSearch(e) {
+        const titleText = e.target.value;
+        setTitle(titleText);
+        onTitleChange(titleText);
+    }
+
+    function handleGenreSearch(e) {
+        const titleText = e.target.value;
+        setTitle(titleText);
+        onGenreChange(titleText);
+    }
+
+    return (
+        <>
+            <input type="text" value={title} onChange={(e)=> {handleTitleSearch(e)}} placeholder="Enter a title"></input>
+            <button onClick={()=>{handleTitleSearch();}}>Search</button>
+            {title}
         </>
     );
 }
